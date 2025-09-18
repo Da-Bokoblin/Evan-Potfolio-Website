dc62bc09483120c6ed0de88df2962e3afe720094
Da-Bokoblin <evan.creason@icloud.com>
2025-09-18 11:26:28 -0400

    final version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Evan Potfolio Website
 joey wants to diddleler diddily did did da do me
+Portfolio project for Capstone class 2025
--- website/pages/script.js
+++ website/pages/script.js
@@ -13,11 +13,11 @@ const projects = [
     new project("a", "../assets/favicon.PNG", "b", "index.html")
 ]
 
-function projectFormat(arg) {
+function projectFormat(arg, arg2) {
     let a = `
     <div class="projectFormat">
     <br>
-    <h2>Featured Project</h2>
+    <h2>${arg2}</h2>
     <img src="${arg.image}"> 
     <br>
     <h3>${arg.name}</h3>
@@ -30,14 +30,14 @@ function projectFormat(arg) {
 
 let featuredNum = Math.floor(Math.random()*projects.length)
 if (document.getElementById("featured") !== null) {
-    document.getElementById("featured").innerHTML = projectFormat(projects[featuredNum])
+    document.getElementById("featured").innerHTML = projectFormat(projects[featuredNum], "Featured Project")
 }
 let i = 0
 if (document.getElementById("projectsDiv") !== null) {
     // document.getElementById("featured").innerHTML = projectFormat(projects[featuredNum])
     while (i < projects.length) {
         console.log("ghdfj")
-        document.getElementById("projectsDiv").innerHTML+= projectFormat(projects[i])
+        document.getElementById("projectsDiv").innerHTML+= projectFormat(projects[i], "")
         i++
     }
 }
